b042a627c4d49a443e74dc5de4313649a7a611de
zcy7227 <815311540@qq.com>
2019-06-13 09:37:53 +0800

    First commit

---
+++ src/main/java/com/mx/patshop/PatshopApplication.java
@@ -0,0 +1,13 @@
+package com.mx.patshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatshopApplication.class, args);
+    }
+
+}
